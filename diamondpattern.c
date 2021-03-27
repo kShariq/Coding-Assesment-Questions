8709ea500b3a8a1aa9797ae304ffb2b318fef9a9
Mohd Shariq Khan <khanshariq1729@gmail.com>
2021-03-27 16:09:09 +0530

    Add files via upload
    printing diamond pattern

---
+++ diamondpattern.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i,j,k,a,b,c;
+    int n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(k=1;k<=n-i;k++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=2*i-1;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=n-1;i>=1;i--)
+    {
+        for(k=1;k<=n-i;k++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=2*i-1;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
