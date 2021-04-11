721bad38bf245ed0fd6e846779b0b5fc89c5597a
Mohd Shariq Khan <khanshariq1729@gmail.com>
2021-04-11 15:22:44 +0530

    Add files via upload
    Current Date Time and Hours code in Python.

---
+++ date time python.py
@@ -0,0 +1,20 @@
+import datetime
+cd=datetime.datetime.now()
+"""
+print(datetime.date.today())
+print(datetime.date.today().year)
+print(datetime.date.today().day)
+print(datetime.date.today().month)
+
+print(cd.strftime('%d %b, %Y'))
+print(cd.strftime('%b %d, %Y'))
+print(cd.strftime('%d %A, %y'))
+print(cd.strftime('Please attend our event %A, %b %d in the Year %Y'))
+print(cd.strftime('%B %A, %y'))
+"""
+
+print(cd)
+print(cd.hour)
+print(cd.minute)
+print(cd.seconds)
+
